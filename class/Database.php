4639510041bd2874b6d4cd07bd8ea4fcc969da8d
Konradx3 <96784610+Konradx3@users.noreply.github.com>
2022-08-24 15:56:00 +0200

    Create Database, many tasks from mechanics

---
+++ class/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database
+{
+    private $host = 'localhost';
+    private $dbname = 'shorturl';
+    private $user = 'root';
+    private $password = '';
+
+    public function Connect()
+    {
+        return new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->user, $this->password);
+    }
+    public function Insert($oldUrl, $newUrl)
+    {
+        $query = "INSERT INTO `links`(`oldUrl`, `newUrl`, `date`) 
+                  VALUES (:old, :new, now())";
+        $stmt = $this->Connect()->prepare($query);
+        $stmt->bindParam(':old', $oldUrl);
+        $stmt->bindParam(':new', $newUrl);
+        return $stmt->execute();
+    }
+    public function Show($newUrl)
+    {
+        $query = "SELECT * FROM `links` 
+                  WHERE `newUrl` = ?";
+        $stmt = $this->Connect()->prepare($query);
+        $stmt->execute([$newUrl]);
+        return $stmt->fetch();
+    }
+}
+
--- main.php
+++ main.php
@@ -1,13 +1,16 @@
 <?php
 require_once ('class/GeneratorNewId.php');
+require_once ('class/Database.php');
 
 if (!empty($_POST['url']))
 {
     if (preg_match('|^http(s)?://[a-z\d-]+(.[a-z\d-]+)*(:\d+)?(/.*)?$|i', $_POST['url']))
     {
-        $newUrl = new GeneratorNewId();
         try {
-            $newUrl->uniqidReal(10);
+            $newUrl = new GeneratorNewId();
+            $insert = new Database();
+            $insert->Insert($_POST['url'], $newUrl->uniqidReal(10));
+            header('Location: thanks.php');
         } catch (Exception $e) {echo 'Error';}
     }
     else
